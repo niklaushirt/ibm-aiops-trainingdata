42a1028da086e2eb4cb83f519a45f02311c94b30
Niklaus Hirt <nikh@ch.ibm.com>
2023-10-26 16:31:49 +0200

    Metricsa Data till Jan 24

--- training-data/template/AdjustDates.sh
+++ training-data/template/AdjustDates.sh
@@ -1,7 +1,7 @@
-sed -i -e 's/2023-01-/2023-08-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-02-/2023-09-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-03-/2023-10-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-04-/2023-11-/g' $(find $(pwd)|grep .csv)
+sed -i -e 's/2023-01-/2023-10-/g' $(find $(pwd)|grep .csv)
+sed -i -e 's/2023-02-/2023-11-/g' $(find $(pwd)|grep .csv)
+sed -i -e 's/2023-03-/2023-12-/g' $(find $(pwd)|grep .csv)
+sed -i -e 's/2023-04-/2024-01-/g' $(find $(pwd)|grep .csv)
 
 rm *csv-e*
 
