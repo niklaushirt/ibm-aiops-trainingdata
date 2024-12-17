4cdb09a8b017e0ed4e4eea5abf66b30e726e1a54
Niklaus Hirt <nikh@ch.ibm.com>
2024-12-17 09:45:06 +0100

    Yearly Metrics

--- training-data/AdjustDates.sh
+++
@@ -1,6 +0,0 @@
-sed -i -e 's/2023-01-/2024-01-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-02-/2024-02-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-03-/2024-03-/g' $(find $(pwd)|grep .csv)
-sed -i -e 's/2023-04-/2024-04-/g' $(find $(pwd)|grep .csv)
-
-rm *csv-e*
